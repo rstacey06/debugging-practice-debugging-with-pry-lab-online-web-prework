c31d7bf13635b658e0b048dc2b043ba06b02e70a
burbage stacey <rstacey06@gmail.com>
2019-10-25 21:52:48 +0000

    Done.

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,29 +1,30 @@
 class Person
   attr_accessor :name, :candy_lover
-
+  
   def initialize(name, candy_lover)
     @name = name
     @candy_lover = candy_lover
   end
 
   def tall?
-    true
+     return true
   end
 
   def basketball_player?
-    false
+    return false
   end
 
   def likes_candy?
-    true
+    return true
   end
 
   def ballin_candy_lover?
     if @candy_lover == true
-      "Aw sweet, this cat loves them some candy!"
+      p "Aw sweet, this cat loves them some candy!"
     else
       10.times do
-        "not a ballin candy lover"
+        p "not a ballin candy lover"
+      end
     end
   end
 end
